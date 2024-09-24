96421805b25d0c73c3ade30008adcc87651d9d52
Gushchin Pavel <pavel.gushchin.2005@gmail.com>
2024-09-24 18:46:24 +0300

    Add Account

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class Account {
+import java.util.Date;
+
+public class Account
+{
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    public Account()
+    {
+        this.id = 0;
+        this.balance = 0;
+        this.annualInterestRate = 0;
+    }
 }
