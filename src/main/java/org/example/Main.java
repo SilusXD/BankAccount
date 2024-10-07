bdb82288ea0f22629ef4208554a89e9ab1a9d167
Moroz Ekaterina <katya.moroz.05@inbox.ru>
2024-10-07 15:26:27 +0300

    Update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,8 @@ public class Main {
         Account[] mass = new Account[10];
         for (int i = 0; i < 10; i++)
         {
-            mass[i] = new Account(i, 10000);
+            mass[i] = new Account("user" + i, i, 10000);
+            mass[i].setAnnualInterestRate(7.5);
         }
 
         int id;
@@ -40,7 +41,8 @@ public class Main {
             System.out.println("1: проверить баланс счета");
             System.out.println("2: снять со счета");
             System.out.println("3: положить на счет");
-            System.out.println("4: выйти");
+            System.out.println("4: отобразить сводку по счету");
+            System.out.println("5: выйти");
             System.out.print("Введите пункт меню: "); numOfPunkt = in.nextInt();
 
 
@@ -51,22 +53,40 @@ public class Main {
             else if(numOfPunkt == 2)
             {
                 double money;
+                String description;
 
                 System.out.print("Введите сумму для снятия: ");
                 money = in.nextDouble();
+                in.nextLine();
+                System.out.print("Введите комментарий к транзакции: ");
+                description = in.nextLine();
 
-                mass[id].withdraw(money);
+                mass[id].withdraw(money, description);
             }
             else if(numOfPunkt == 3)
             {
                 double money;
+                String description;
 
                 System.out.print("Введите сумму для пополнения: ");
                 money = in.nextDouble();
+                in.nextLine();
+                System.out.print("Введите комментарий к транзакции: ");
+                description = in.nextLine();
 
-                mass[id].deposit(money);
+                mass[id].deposit(money, description);
             }
             else if(numOfPunkt == 4)
+            {
+                System.out.println("Владелец: " + mass[id].getName());
+                System.out.println("Процентная ставка: " + mass[id].getAnnualInterestRate() + "%");
+                System.out.println("Баланс: " + mass[id].getBalance());
+                System.out.println();
+
+                System.out.println("Транзакции: ");
+                mass[id].displayTransactions();
+            }
+            else if(numOfPunkt == 5)
             {
                 break;
             }
